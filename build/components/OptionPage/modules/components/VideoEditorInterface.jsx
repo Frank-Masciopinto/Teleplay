51382d54d913bf09f9367e8553a521c88b0d6dea
Frank-Masciopinto <78543213+Frank-Masciopinto@users.noreply.github.com>
2024-01-20 16:10:26 +0100

    rendering overlay correctly

--- build/components/OptionPage/modules/components/VideoEditorInterface.jsx
+++ build/components/OptionPage/modules/components/VideoEditorInterface.jsx
@@ -13,6 +13,9 @@ function VideoEditorInterface({
 }) {
   const [loaded, setLoaded] = useState(false);
   const [videoBounds, setVideoBounds] = useState(null);
+  const [overlayPositions, setOverlayPositions] = useState([]);
+  const [scaleX, setScaleX] = useState(1); // Horizontal scaling factor
+  const [scaleY, setScaleY] = useState(1); // Vertical scaling factor
   const ffmpegRef = useRef(new FFmpeg());
   const messageRef = useRef(null);
   const load_FFmpeg = async () => {
@@ -31,25 +34,33 @@ function VideoEditorInterface({
 
     setLoaded(true);
   };
+
   const handleMetadataLoaded = () => {
     const videoElement = videoRef.current;
     if (videoElement) {
-      const width = videoElement.videoWidth;
-      const height = videoElement.videoHeight;
-      console.log(`Video dimensions: ${width}x${height}`);
-      const aspectRatio = width / height;
+      const originalWidth = videoElement.videoWidth;
+      const originalHeight = videoElement.videoHeight;
+      console.log(
+        `Original Video dimensions: ${originalWidth}x${originalHeight}`
+      );
+
+      const aspectRatio = originalWidth / originalHeight;
       const containerHeight = window.innerHeight * 0.7; // 70vh
       const newWidth = containerHeight * aspectRatio;
+
       videoElement.parentElement.style.width = `${newWidth}px`;
 
-      // Construct your FFmpeg command here
-      // For example, position the overlay at the bottom right
-      // let posX = width - overlayWidth - 10;
-      // let posY = height - overlayHeight - 10;
-      // let ffmpegCommand = `ffmpeg.exec -i input_video.mp4 -i overlay_image.png -filter_complex "overlay=${posX}:${posY}" -codec:a copy output_video.mp4`;
-      // console.log("FFmpeg Command:", ffmpegCommand);
+      // Calculate and update scaling factors
+      const renderedWidth = videoElement.clientWidth;
+      const renderedHeight = videoElement.clientHeight;
+      console.log("renderedWidth", renderedWidth);
+      console.log("scaled width", originalWidth / renderedWidth);
+      console.log("renderedHeight", renderedHeight);
+      setScaleX(originalWidth / renderedWidth);
+      setScaleY(originalHeight / renderedHeight);
     }
   };
+
   useEffect(() => {
     // Initialize video.js player
     load_FFmpeg();
@@ -70,7 +81,13 @@ function VideoEditorInterface({
       }
     };
   }, [videoRef, playerRef]);
-
+  async function writeBlobToFFmpegFS(path, blobUrl) {
+    const response = await fetch(blobUrl);
+    const blob = await response.blob();
+    const arrayBuffer = await blob.arrayBuffer();
+    const uint8Array = new Uint8Array(arrayBuffer);
+    await ffmpegRef.current.writeFile(path, uint8Array);
+  }
   async function renderVideo() {
     console.log("☘️ renderVideo");
     const ffmpeg = ffmpegRef.current;
@@ -79,30 +96,88 @@ function VideoEditorInterface({
     const videoData = await fetchFile(videoFile);
     await ffmpeg.writeFile("input.mp4", videoData); // Fetch and write the overlay image to FFmpeg's filesystem
 
-    // Example FFmpeg command (replace with your desired editing operations)
-    await ffmpeg.exec([
+    // Construct the overlay filter part
+    let overlayFilters = overlayPositions
+      .map((overlay, index) => {
+        return `[0:v][${index + 1}:v]overlay=${overlay.x}:${overlay.y}`;
+      })
+      .join(";");
+
+    // Include the overlay inputs and the filter in the FFmpeg command
+    const ffmpegCommand = [
       "-hwaccel",
-      "auto", // Use hardware acceleration if available
+      "auto",
       "-i",
       "input.mp4",
-      "-vf",
-      "transpose=1",
+      // Include each overlay as an input
+      ...overlayPositions.map((overlay) => ["-i", "overlay0.png"]).flat(),
+      "-filter_complex",
+      overlayFilters, // Add the overlay filters here
       "-c:v",
-      "libx264", // Use a specific encoder, change as needed
+      "libx264",
       "-preset",
-      "fast", // Faster encoding with slightly higher file size
+      "fast",
       "output.mp4",
-    ]);
+    ];
+    console.log("⭐⭐⭐ffmpegCommand");
+    console.log(ffmpegCommand);
+    // Example FFmpeg command (replace with your desired editing operations)
+    await ffmpeg.exec(ffmpegCommand);
     // Read the edited video file
     const data = await ffmpeg.readFile("output.mp4");
     let edited_video_url = URL.createObjectURL(
       new Blob([data.buffer], { type: "video/mp4" })
     );
     setVideoFile(null);
-    console.log("Finished editing editedVideoUrl");
+    console.log("🟢 Finished editing editedVideoUrl 🟢");
     console.log(edited_video_url);
     setVideoFile(edited_video_url);
   }
+  const handleStop = async (index, e, data, overlayPath) => {
+    console.log(`Original Position - X: ${data.x}, Y: ${data.y}`);
+    console.log(`Scaling Factors - scaleX: ${scaleX}, scaleY: ${scaleY}`);
+    // Assuming videoRef is a reference to your video element
+    const videoElement = videoRef.current;
+    const parentWidth = videoElement.parentElement.offsetWidth;
+    const parentHeight = videoElement.parentElement.offsetHeight;
+
+    // Calculate the offset from the center to the top-left corner
+    const offsetX = parentWidth / 2;
+    const offsetY = parentHeight / 2;
+
+    // Adjust the coordinates
+    const adjustedX = (data.x + offsetX) * scaleX;
+    const adjustedY = data.y * scaleY;
+
+    // Ensure coordinates are positive
+    const finalX = Math.max(0, adjustedX);
+    const finalY = Math.max(0, adjustedY);
+
+    console.log(`Adjusted Position - X: ${finalX}, Y: ${finalY}`);
+    console.log("index", index);
+    const overlayFileName = `overlay${index}.png`;
+
+    // Write the blob URL to the FFmpeg file system with the defined filename
+    await writeBlobToFFmpegFS(overlayFileName, overlayPath);
+    setOverlayPositions((prevPositions) => {
+      const found = prevPositions.some(
+        (item) => item.overlayPath === overlayPath
+      );
+
+      if (found) {
+        return prevPositions.map((item) =>
+          item.overlayPath === overlayPath
+            ? { ...item, x: finalX, y: finalY }
+            : item
+        );
+      } else {
+        return [
+          ...prevPositions,
+          { overlayPath: overlayPath, x: adjustedX, y: adjustedY },
+        ];
+      }
+    });
+  };
 
   return (
     <div>
@@ -112,7 +187,11 @@ function VideoEditorInterface({
         </video>
         {videoBounds &&
           overlays.map((overlay, index) => (
-            <Draggable key={index} bounds="parent">
+            <Draggable
+              key={index}
+              bounds="parent"
+              onStop={(e, data) => handleStop(index, e, data, overlay)}
+            >
               <img
                 src={overlay}
                 className="overlay-image"
